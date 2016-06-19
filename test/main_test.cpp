a0735baf48c5669162024d9a624daeb9b05e4c01
Daouzli A <daouzli@gmail.com>
2016-06-19 21:30:20 +0200

    add CATCH unit test framework and add first tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,9 +1,50 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include <iostream>
+#include <sstream>
 #include "BinMaker.h"
 
-int main(void)
+
+TEST_CASE( "Check fail to make binary", "[binmake]" )
 {
+
+    SECTION( "- don't get binary if no input available" )
+    {
+        BinMaker b;
+        std::vector<char> output;
+        REQUIRE( b.get_binary(output) == false );
+    }
+
+}
+
+TEST_CASE( "Check succeed to make binary", "[binmake]" )
+{
+
     BinMaker b;
-    std::cout << "TODO tests" << std::endl;
-    return 0;
+    std::stringstream ss;
+    std::vector<char> output;
+    ss << "big-endian" << std::endl
+            << "00010203" << std::endl
+            << "little-endian" << std::endl
+            << "04050607" << std::endl;
+    b.set_input(ss);
+
+    SECTION( "- make a binary from a stringstream" )
+    {
+        REQUIRE( b.make_binary() == true );
+    }
+
+    SECTION( "- made binary endianess valid" )
+    {
+        REQUIRE( b.get_binary(output) == true );
+        REQUIRE( output.size() == 8);
+        REQUIRE( output[0] == 0x00 );
+        REQUIRE( output[1] == 0x01 );
+        REQUIRE( output[2] == 0x02 );
+        REQUIRE( output[3] == 0x03 );
+        REQUIRE( output[4] == 0x07 );
+        REQUIRE( output[5] == 0x06 );
+        REQUIRE( output[6] == 0x05 );
+        REQUIRE( output[7] == 0x04 );
+    }
 }
