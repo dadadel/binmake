0a220bd740399a8a2d722296d436956e775066e0
Daouzli A <daouzli@gmail.com>
2017-01-19 23:23:41 +0100

    update tests (compiling and running)

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -2,70 +2,76 @@
 #include "catch.hpp"
 #include <iostream>
 #include <sstream>
-#include "BinMaker.h"
+#include "BinStream.h"
 
+using namespace BS;
 
-TEST_CASE( "Check fail to make binary", "[binmake]" )
+TEST_CASE( "Check failure to make binary", "[binmake]" )
 {
 
     SECTION( "- don't get binary if no input available" )
     {
-        BinMaker b;
+        BinStream b;
         std::vector<char> output;
-        REQUIRE( b.get_binary(output) == false );
+        REQUIRE( b.get_output(output) == false );
     }
 
 }
 
-TEST_CASE( "Check succeed to make binary", "[binmake]" )
+TEST_CASE( "Check success to make binary", "[binmake]" )
 {
 
-    BinMaker b;
+    BinStream b;
     std::stringstream ss;
     std::vector<char> output;
-    ss << "big-endian" << std::endl
-            << "00010203" << std::endl
-            << "little-endian" << std::endl
-            << "04050607" << std::endl;
-    b.set_input(ss);
 
-    SECTION( "- make a binary from a stringstream" )
+    SECTION( "- get input from a stringstream" )
     {
-        REQUIRE( b.make_binary() == true );
+        ss << "big-endian"
+            << " 00010203"
+            << " little-endian"
+            << " 04050607";
+        b << ss;
+        REQUIRE( b.input_ready() == true );
+    }
+
+    SECTION( "- produced an output binary from a stringstream" )
+    {
+        REQUIRE( b.output_ready() == true );
     }
 
     SECTION( "- made binary endianess valid" )
     {
-        REQUIRE( b.get_binary(output) == true );
-        REQUIRE( output.size() == 8);
-        REQUIRE( output[0] == 0x00 );
-        REQUIRE( output[1] == 0x01 );
-        REQUIRE( output[2] == 0x02 );
-        REQUIRE( output[3] == 0x03 );
-        REQUIRE( output[4] == 0x07 );
-        REQUIRE( output[5] == 0x06 );
-        REQUIRE( output[6] == 0x05 );
-        REQUIRE( output[7] == 0x04 );
+        REQUIRE( b.size() == 8);
+        REQUIRE( b[0] == 0x00 );
+        REQUIRE( b[1] == 0x01 );
+        REQUIRE( b[2] == 0x02 );
+        REQUIRE( b[3] == 0x03 );
+        REQUIRE( b[4] == 0x07 );
+        REQUIRE( b[5] == 0x06 );
+        REQUIRE( b[6] == 0x05 );
+        REQUIRE( b[7] == 0x04 );
+    }
+
+    SECTION( "- reset the data" )
+    {
+        b.reset();
+        REQUIRE( b.input_ready() == false);
+        REQUIRE( b.output_ready() == false);
     }
 
     SECTION( "- make a binary from an explicit hexadecimal number" )
     {
-        ss.str("");
-        ss.clear();
-        ss << "big-endian" << std::endl <<
-                "%x12345678" << std::endl <<
-                "%x2f %x00" << std::endl;
-        b.set_input(ss);
-        b.reset_output();
-        REQUIRE( b.make_binary() == true );
-        REQUIRE( b.get_binary(output) == true );
-        REQUIRE( output.size() == 6);
-        REQUIRE( output[0] == 0x12 );
-        REQUIRE( output[1] == 0x34 );
-        REQUIRE( output[2] == 0x56 );
-        REQUIRE( output[3] == 0x78 );
-        REQUIRE( output[4] == 0x2f );
-        REQUIRE( output[5] == 0x00 );
+        b << "big-endian" <<
+                "%x12345678" <<
+                "%x2f %x00";
+        REQUIRE( b.size() == 6);
+        REQUIRE( b[0] == 0x12 );
+        REQUIRE( b[1] == 0x34 );
+        REQUIRE( b[2] == 0x56 );
+        REQUIRE( b[3] == 0x78 );
+        REQUIRE( b[4] == 0x2f );
+        REQUIRE( b[5] == 0x00 );
     }
     //TODO test hex zero padded size
 }
